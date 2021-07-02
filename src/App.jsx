80b72d37311a4b4d284289a71c70b5a7f7c05b54
Franco Carames <franco.carames1@gmail.com>
2021-07-01 23:28:08 -0300

    Navbar + estilos

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import './index.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <body>
+      <Navbar/>
+    </body>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className="barra-nav">
+            <p className="nombre-tienda activa">Mundo Otaku</p>
+            <ul className="lista-plegable">
+                <li className="contenido-lista">Mangas</li>
+                <li className="contenido-lista">Indumentaria</li>
+                <li className="contenido-lista">Accesorios</li>
+                <li className="contenido-lista">Decoraciones</li>
+                <li className="contenido-lista">Contacto</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
