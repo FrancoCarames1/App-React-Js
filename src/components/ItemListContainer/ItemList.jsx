6723ee452ef226c34a30152cb04c21fe727269c9
Franco Carames <franco.carames1@gmail.com>
2021-07-18 12:09:50 -0300

    Corrección de llamado de ItemCard

--- src/components/ItemListContainer/ItemList.jsx
+++ src/components/ItemListContainer/ItemList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ItemCount from './ItemCard/ItemCard';
+import ItemCard from './ItemCard/ItemCard';
 
 const ItemList = ({displayItems}) => {
     return(
         <>
             {displayItems.map((item) => (
-                <ItemCount item={item} key={item.id}/>
+                <ItemCard item={item} key={item.id}/>
             ))}
         </>
     );
