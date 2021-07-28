3bf69c290467c834c890f2ed15bcaf48857c02db
FrancoCarames1 <franco.carames1@gmail.com>
2021-07-28 14:36:43 -0300

    Corrección

---
+++ src/service/Cart-Context.jsx
@@ -0,0 +1,55 @@
+import React, {useState, useEffect} from 'react';
+import { useMemo } from 'react';
+
+const CartContext = React.createContext();
+
+export function CartProvider(props) {
+
+    const [cartItems, setCartItems] = useState([]);
+
+    const clearCart = () => {
+
+        setCartItems([]);
+
+    }
+
+    const addCartItem = (itemObject, quantity) => {
+
+        if (cartItems.length === 0){
+            setCartItems.push({id: itemObject.id, productObject: itemObject, productQuantity: quantity});
+        }else{
+            setCartItems.forEach(element => {
+                if(element.id === itemObject.id){
+                    element.productQuantity = quantity;
+                }else{
+                    setCartItems.push({id: itemObject.id, productObject: itemObject, productQuantity: quantity});
+                }
+            });
+        }
+        console.log(setCartItems);
+    }
+
+    const deleteCartItem = (id) => {
+        const positionToDelete = cartItems.findIndex( element => element.id === id);
+        setCartItems.splice(positionToDelete,1);
+    }
+
+    const value = useMemo(() => {
+        return ({
+            cartItems,
+            clearCart,
+            deleteCartItem,
+            addCartItem
+        })
+    }, [cartItems])
+
+    return <CartContext.Provider value={value} {...props} />
+}
+
+export function useCart() {
+    const context = React.useContext(CartContext);
+    if (!context) {
+        throw new Error('useCart debe estar dentro del proveedor CartContext')
+    }
+    return context;
+}
