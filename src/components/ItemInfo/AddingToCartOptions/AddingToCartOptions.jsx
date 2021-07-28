ec42506cd642217170ffb362b6cff737c658abad
FrancoCarames1 <franco.carames1@gmail.com>
2021-07-28 18:46:27 -0300

    Error que no pasa información del producto

--- src/components/ItemInfo/AddingToCartOptions/AddingToCartOptions.jsx
+++ src/components/ItemInfo/AddingToCartOptions/AddingToCartOptions.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useCart } from "../../../service/Cart-Context";
 
-const AddingToCartOptions = ({invertirEstado, item, contador}) =>{
+const AddingToCartOptions = ({invertirEstado, itemProducto, cantidadProducto}) =>{
 
     const {addCartItem} = useCart();
 
     return(
         <div className="contenedor-opciones">
-            <Link to="/" onClick={addCartItem.bind(item, contador)}>Agregar y seguir comprando</Link>
+            <Link to="/" onClick={addCartItem.bind(itemProducto, cantidadProducto)}>Agregar y seguir comprando</Link>
             <button onClick={invertirEstado}>Modificar cantidad</button>
-            <Link to="/cart" onClick={addCartItem.bind(item, contador)}>Finalizar compras</Link>
+            <Link to="/cart" onClick={addCartItem.bind(itemProducto, cantidadProducto)}>Finalizar compras</Link>
         </div>
     )
 }
